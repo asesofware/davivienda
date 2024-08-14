caa377bf43e5918715716653b90470b0be631481
Jhonnatan Mayorga <jhonnatan.mayorga@digiware.net>
2024-08-14 16:34:47 -0500

    primera version metodos de pago

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,27 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    const metodosPago = {
+      "code": "SUCCESS",
+      "error": null,
+      "paymentMethods": [
+          {
+              "id": "36",
+              "description": "EFECTY",
+              "country": "CO",
+              "enabled": true,
+              "reason": null
+          },
+          {
+              "id": "10",
+              "description": "MASTERCARD",
+              "country": "co",
+              "enabled": true,
+              "reason": null
+          }
+      ]
+  }
+    return metodosPago;
   }
 }
